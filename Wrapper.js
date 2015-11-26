86a6110993597286d19bedb34e45a7092aba74e6
Romén Rodríguez <romenrg@gmail.com>
2015-11-26 17:17:42 +0000

    First test is passing, the first newline is added in the right place, replacing the closest space to the maximum column.

--- Wrapper.js
+++ Wrapper.js
@@ -2,7 +2,17 @@ console.log("Code Kata: Word Wrap, by @romenrg");
 
 function Wrapper() {
     this.wrap = function(string, columnNum) {
-        return string;
+        var lastBlankPos = null;
+        var newString = null;
+        for (var i=0; i<columnNum; i++) {
+            if (string[i] == ' ') {
+                lastBlankPos = i;
+            }
+        }
+        if (lastBlankPos != null) {
+            newString = string.substr(0, lastBlankPos) + '\n' + string.substr(lastBlankPos+1, string.length);
+        }
+        return newString;
     }
 }
 
