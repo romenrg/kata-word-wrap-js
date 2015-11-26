9f094ad85602c2b05abce641e2f0213a5485dcc2
Romén Rodríguez <romenrg@gmail.com>
2015-11-26 17:24:15 +0000

    Adding a new test in which two newlines should be added, replacing two blank spaces

--- Wrapper.spec.js
+++ Wrapper.spec.js
@@ -8,5 +8,11 @@ describe('Wrapper', function () {
         var maxColumn = 14;
         var expectedResult = "This is the\nfirst test";
         expect(wrapper.wrap(testString, maxColumn)).to.equal(expectedResult);
+    });
+    it('should do two replacements', function() {
+        var testString = "This is the second test, with 3 lines";
+        var maxColumn = 14;
+        var expectedResult = "This is the\nsecond test,\nwith 3 lines";
+        expect(wrapper.wrap(testString, maxColumn)).to.equal(expectedResult);
     })
 });
