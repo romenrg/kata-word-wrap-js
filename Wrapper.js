e5f99313ed6c9c94c3049c6442f84bc29708d75f
Romén Rodríguez <romenrg@gmail.com>
2015-11-26 18:02:49 +0000

    Test with text that needs to have two replacements is passing now

--- Wrapper.js
+++ Wrapper.js
@@ -2,15 +2,30 @@ console.log("Code Kata: Word Wrap, by @romenrg");
 
 function Wrapper() {
     this.wrap = function(string, columnNum) {
+        var tmpString = string;
+        var newString = "";
         var lastBlankPos = null;
-        var newString = null;
-        for (var i=0; i<columnNum; i++) {
-            if (string[i] == ' ') {
-                lastBlankPos = i;
+        var finished = false;
+        while (!finished) {
+            console.log("tmp is: "+tmpString);
+            lastBlankPos = null;
+            for (var i=0; i<columnNum; i++) {
+                if (tmpString.length > i) {
+                    if (tmpString[i] == ' ') {
+                        lastBlankPos = i;
+                    }
+                }
+                else {
+                    finished = true;
+                }
             }
-        }
-        if (lastBlankPos != null) {
-            newString = string.substr(0, lastBlankPos) + '\n' + string.substr(lastBlankPos+1, string.length);
+            if ((lastBlankPos != null) && (!finished)) { //TODO: Be careful with cases in which there are no spaces
+                newString = newString.concat(tmpString.substr(0, lastBlankPos) + '\n');
+                tmpString = tmpString.substr(lastBlankPos+1, tmpString.length);
+            }
+            else {
+                newString = newString.concat(tmpString.substr(0, tmpString.length));
+            }
         }
         return newString;
     }
