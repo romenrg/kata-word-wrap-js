5514acdf03b1b79266802b73817abffc89c54170
Romén Rodríguez <romenrg@gmail.com>
2015-11-29 18:31:45 +0000

    Refactoring the code section that calculates the last blank position (and that also detects if the text has been completely processed).

--- Wrapper.js
+++ Wrapper.js
@@ -2,30 +2,35 @@ function Wrapper() {
     this._areParamTypesValidForWrapFunction = function(text, columnNum) {
         return (typeof(text) == "string") && (typeof(columnNum) == "number") && (columnNum > 0);
     };
+    this._calculateLastBlankPos = function(text, columnNum) {
+        var finished = false;
+        var lastBlankPos = null;
+        for (var i=0; i<columnNum+1; i++) {
+            if (text.length > i) {
+                if (text[i] == ' ') {
+                    lastBlankPos = i;
+                }
+            }
+            else {
+                finished = true;
+            }
+        }
+        return {finished: finished, lastBlankPos: lastBlankPos};
+    };
     this.wrap = function(text, columnNum) {
         if (this._areParamTypesValidForWrapFunction(text, columnNum)) {
             var tmpString = text;
             var newString = "";
-            var finished = false;
-            while (!finished) {
-                var lastBlankPos = null;
-                for (var i=0; i<columnNum+1; i++) {
-                    if (tmpString.length > i) {
-                        if (tmpString[i] == ' ') {
-                            lastBlankPos = i;
-                        }
-                    }
-                    else {
-                        finished = true;
-                    }
+            var processing = {finished: false, lastBlankPos: null};
+            while (!processing.finished) {
+                processing = this._calculateLastBlankPos(tmpString, columnNum);
+                if ((!processing.finished) && (processing.lastBlankPos != null)) {
+                    newString = newString.concat(tmpString.substr(0, processing.lastBlankPos) + '\n');
+                    tmpString = tmpString.substr(processing.lastBlankPos+1, tmpString.length);
                 }
-                if ((!finished) && (lastBlankPos != null)) {
-                    newString = newString.concat(tmpString.substr(0, lastBlankPos) + '\n');
-                    tmpString = tmpString.substr(lastBlankPos+1, tmpString.length);
-                }
-                else if (finished) {
+                else if (processing.finished) {
                     newString = newString.concat(tmpString.substr(0, tmpString.length));
-                    finished = true;
+                    processing.finished = true;
                 }
                 else {
                     newString = newString.concat(tmpString.substr(0, columnNum) + '\n');
